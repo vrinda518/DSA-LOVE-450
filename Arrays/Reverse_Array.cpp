3a451203f366dc5468f7106cd9045c8da0c863d3
Vrinda Agarwal <vrinda4879@gmail.com>
2022-01-07 12:35:52 +0530

    First commit

---
+++ Arrays/Reverse_Array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseArray(vector<int> &arr , int m)
+{
+	// Write your code here.
+    int n = arr.size();
+    int i = m+1, j = n-1;
+    while (i<=j)
+    {
+        int temp = arr[i];
+
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--;
+    }
+
+}
+
+int main()
+{
+  std::vector<int> v = {1,2,3,4,5,6};
+  reverseArray(v,3);
+  for (auto ele : v)
+    cout<<ele<<" ";
+  return 0;
+}
